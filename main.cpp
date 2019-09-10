936ff6a800018d696f4da51961703c90aa126bb0
joshDavy <54147300+joshDavy@users.noreply.github.com>
2019-09-10 13:19:06 +0100

    Inital Commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/*
+ELEC2645 Embedded Systems Project
+School of Electronic & Electrical Engineering
+University of Leeds
+Name: 
+Username: 
+Student ID Number: 
+Date: XX/XX/XXXX
+*/
+
+// Includes
+#include "mbed.h"
+#include "Gamepad.h"
+#include "N5110.h"
+#include "stdio.h"
+#include "music.h"
+#include "soundData.h"
+#include <queue>
+
+// objects
+Gamepad pad;
+N5110 lcd;
+Music music;
+
+int main()
+{
+    // Initalise objects
+    lcd.init();
+    pad.init();
+    music.init(sound_data,NUM_ELEMENTS);
+   
+   // Main loop
+    while (1) {
+        pad.leds_on();
+        pad.tone(1000,1);
+        wait(1);
+        pad.leds_off();
+        pad.tone(1500,1);
+        wait(1);
+    }
+        
+}
